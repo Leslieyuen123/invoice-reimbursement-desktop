baf5534b52e0b5f8dd3e50ad78de56f387d60611
leslie_yuen <leslie_yuen@leslieyendeMBP5.lan>
2026-07-13 20:27:40 +0800

    chore: harden scaffold tooling

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist/**", "src-tauri/target/**", "src-tauri/gen/**"] },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
